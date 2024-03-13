5e90888596d4835091f8ff495407a9f9f3d49f93
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2024-03-13 09:35:51 -0500

    Create Login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -15,7 +16,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  viewPassword: 'password' | 'text' = 'password';
 
-  constructor() { }
+  regexEmail: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  form!: FormGroup;
+
+  constructor(private router: Router, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  readonly changeViewPassword = () =>
+    (this.viewPassword =
+      this.viewPassword === 'password' ? 'text' : 'password');
+
+  valideForm = (field: string): boolean =>
+    this.form.controls[field].errors && this.form.controls[field].touched
+      ? true
+      : false;
+
+  back(): void {
+    this.router.navigate(['/']);
+  }
+
+  savedForm(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+  }
+
+  private initForm(): void {
+    this.form = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(this.regexEmail),
+        ],
+      ],
+      password: ['', [Validators.required]],
+    });
+  }
 }
